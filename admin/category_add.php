21811a44842e816d8eaed86e8980c88e96f90662
duyviet1102 <duyvietdao258@gmail.com>
2023-10-31 11:02:29 +0700

    them file

---
+++ admin/category_add.php
@@ -0,0 +1,12 @@
+<div class="admin-content-right">
+            <div class="admin-content-right-category_add">
+                <h1>Thêm danh mục</h1>
+                <form action="POST">
+                    <input type="text" placeholder="Nhập tên danh mục">
+                    <button type="submit">Thêm</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ admin/slider.php
@@ -0,0 +1,27 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="#">Danh mục</a>
+                    <ul>
+                        <li>
+                            <a href="">Thêm danh mục</a>
+                        </li>
+                        <li>
+                            <a href="">Danh sách danh mục</a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">Loại sách</a>
+                <ul>
+                  <li><a href="">Thêm loại Sách</a></li>
+                  <li><a href="">Danh sách sản phẩm</a></li>
+                </ul>
+                </li>
+                <li><a href="#">Sách</a>
+                <ul>
+                    <li><a href="">Thêm sách</a></li>
+                    <li><a href="">Danh sách sản phẩm</a></li>
+                </ul>
+                </li>
+            </ul>
+        </div>
